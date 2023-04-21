3955a56dd0828f31748e358689a9bc2e1f3145a7
Andres Sanabria <84527322+80asv@users.noreply.github.com>
2023-04-21 11:59:20 +0000

    frontend folder & title home page changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -44,10 +44,10 @@ export default function Home() {
             </div>
             <div className="text-center">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                UN MLP HPTA QUE NO ME BRINDO UN CULO
+                Home page
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                APARECIO ESTE PERSONAJE DISQUE MI CUCHO
+                happy pet project
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
